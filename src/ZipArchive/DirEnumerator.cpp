e735f690a7c2082286cb51eb361f8967f5f6a44c
Strahinja Markovic <devnull@localhost>
2009-08-06 01:57:58 +0200

    Applied provided patch for NetBSD support

--- src/ZipArchive/DirEnumerator.cpp
+++ src/ZipArchive/DirEnumerator.cpp
@@ -73,7 +73,7 @@ bool CDirEnumerator::Start(CFileFilter& filter)
 				if (!entry)
 					break;								
 				CZipString path(m_szCurrentDirectory + entry->d_name);
-	#if !defined __APPLE__ && !defined __CYGWIN__
+	#if !defined __APPLE__ && !defined __CYGWIN__ && !defined __NetBSD__ 
 				struct stat64 sStats;
 				if (stat64(path, &sStats) == -1)
 	#else
