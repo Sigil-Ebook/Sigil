6a15b63bc980bf5af5697118e82336a48cedfc9f
Strahinja Markovic <devnull@localhost>
2009-09-06 15:43:45 +0200

    Providing binary installers for Windows AND LINUX (Fixes issue #6)

--- src/Sigil/main.cpp
+++ src/Sigil/main.cpp
@@ -32,9 +32,9 @@ int main( int argc, char *argv[] )
 
     // Specify the plugin folders 
     // (language codecs and image loaders)
-    app.addLibraryPath( "codecs" );
-    app.addLibraryPath( "iconengines" );
-    app.addLibraryPath( "imageformats" );
+    app.addLibraryPath( "lib/codecs" );
+    app.addLibraryPath( "lib/iconengines" );
+    app.addLibraryPath( "lib/imageformats" );
 
     // We write the full path to Sigil's executable
     // in a file in the home folder for calibre interoperability
