5dc26d7fc631594b041b0f88e647cd80a3bb1a25
Strahinja Markovic <devnull@localhost>
2009-12-13 15:41:01 +0100

    Fixed a rare issue with false spaces being inserted into words during import (Fixes issue #139)

--- src/Sigil/BookManipulation/XHTMLDoc.cpp
+++ src/Sigil/BookManipulation/XHTMLDoc.cpp
@@ -130,8 +130,9 @@ QString XHTMLDoc::GetQDomNodeAsString( const QDomNode &node )
     QTextStream stream( &document_text );
     stream.setCodec( "UTF-8" );
 
-    node.save( stream, 1, QDomNode::EncodingFromTextStream );
+    node.save( stream, 0, QDomNode::EncodingFromTextStream );
     document_text.remove( "&#xd;" );
+    document_text.remove( QChar( '\n' ) );
 
     QRegExp xml_declaration( XML_DECLARATION );
     xml_declaration.setMinimal( true );
