7a988d059f591dbc1219a726243e5efac7e83d73
Strahinja Markovic <devnull@localhost>
2011-02-11 12:21:31 +0100

    Adding an OPFResource class and an NCXResource

---
+++ src/Sigil/ResourceObjects/NCXResource.h
@@ -0,0 +1,46 @@
+/************************************************************************
+**
+**  Copyright (C) 2009, 2010, 2011  Strahinja Markovic  <strahinja.markovic@gmail.com>
+**
+**  This file is part of Sigil.
+**
+**  Sigil is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Sigil is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Sigil.  If not, see <http://www.gnu.org/licenses/>.
+**
+*************************************************************************/
+
+#pragma once
+#ifndef NCXRESOURCE_H
+#define NCXRESOURCE_H
+
+#include "Resource.h"
+
+
+class NCXResource : public Resource 
+{
+    Q_OBJECT
+
+public:
+
+    /**
+     * Constructor.
+     *
+     * @param fullfilepath The full path to the file that this
+     *                     resource is representing.
+     * @param parent The object's parent.
+     */
+    NCXResource( const QString &fullfilepath, QObject *parent = NULL );
+
+};
+
+#endif // NCXRESOURCE_H
