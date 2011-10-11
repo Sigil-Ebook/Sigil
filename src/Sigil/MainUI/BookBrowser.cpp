a61984807441bc598f4ef5bca9d13e1cc17293b4
John Schember <john@nachtimwald.com>
2011-10-11 07:52:17 -0400

    Rename some menu items.

--- src/Sigil/MainUI/BookBrowser.cpp
+++ src/Sigil/MainUI/BookBrowser.cpp
@@ -438,7 +438,7 @@ void BookBrowser::SetupTreeView()
 
 void BookBrowser::CreateContextMenuActions()
 {
-    m_AddNew                  = new QAction( tr( "Add New Item" ),          this );
+    m_AddNew                  = new QAction( tr( "Add Blank Item" ),        this );
     m_AddExisting             = new QAction( tr( "Add Existing Items..." ), this );
     m_Rename                  = new QAction( tr( "Rename" ),                this );
     m_Remove                  = new QAction( tr( "Remove" ),                this );
