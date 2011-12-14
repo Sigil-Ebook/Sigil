d22fc4f1a905fb41c78d547dd31952ea2d2e65f1
John Schember <john@nachtimwald.com>
2011-12-13 19:52:19 -0500

    Fix crash when doing replace with named groups.

--- src/Sigil/PCRE/PCREReplaceTextBuilder.cpp
+++ src/Sigil/PCRE/PCREReplaceTextBuilder.cpp
@@ -186,7 +186,7 @@ bool PCREReplaceTextBuilder::BuildReplacementText(SPCRE &sre,
                             // Check if there is we have a back reference we can
                             // actually get.
                             if (backref_number >= 0 && backref_number < capture_groups_offsets.count()) {
-                                accumulateReplcementText(Utility::Substring(capture_groups_offsets.at(i).first, capture_groups_offsets.at(i).second, text));
+                                accumulateReplcementText(Utility::Substring(capture_groups_offsets.at(backref_number).first, capture_groups_offsets.at(backref_number).second, text));
                             }
                             else {
                                 accumulateReplcementText(invalid_contol);
