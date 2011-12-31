54dea6bc1fd32f34cae1564ccd81d7be5f9c453f
John Schember <john@nachtimwald.com>
2011-12-30 20:26:11 -0500

    Use better en dict which supports fancy quotes. Remove 50 line check limit for getting dictionary encoding because some have it much later in the file.

--- src/Sigil/Misc/SpellCheck.cpp
+++ src/Sigil/Misc/SpellCheck.cpp
@@ -139,15 +139,10 @@ void SpellCheck::setDictionary(const QString &name, bool forceReplace)
     QFile affFile(aff);
     if (affFile.open(QIODevice::ReadOnly)) {
         QTextStream affStream(&affFile);
-        QRegExp encDetector("SET\\s+(.+)", Qt::CaseInsensitive);
-        int count = 0;
+        QRegExp encDetector("^\\s*SET\\s+(.+)", Qt::CaseInsensitive);
         for (QString line = affStream.readLine(); !line.isNull(); line = affStream.readLine()) {
             if (line.contains(encDetector)) {
                 encoding = encDetector.cap(1).trimmed();
-                break;
-            }
-            // Only look at the first 50 lines of the file.
-            if (count++ >= 50) {
                 break;
             }
         }
