28c71577564be9e2ebee66e1d241bbf191650f93
John Schember <john@nachtimwald.com>
2012-03-29 21:21:47 -0400

    Remove unnecessary items from BV toolbar.

--- src/Sigil/Resource_Files/javascript/ckeditor/config.js
+++ src/Sigil/Resource_Files/javascript/ckeditor/config.js
@@ -5,11 +5,10 @@
 	[
 	    
 	    { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord' ] },
-	    { name: 'editing', items : [ 'Find','Replace','-','SelectAll'] },
 	    { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
     	{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv', '-', 'JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
     	{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
-    	{ name: 'insert', items : [ 'Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe' ] },
+    	{ name: 'insert', items : [ 'Table','HorizontalRule','SpecialChar','PageBreak' ] },
     	{ name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
     	{ name: 'colors', items : [ 'TextColor','BGColor' ] },
 	]
