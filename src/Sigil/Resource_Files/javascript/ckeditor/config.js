aca0d34532d2aed516c7cf0aeec19ea64288421c
John Schember <john@nachtimwald.com>
2012-04-08 09:22:07 -0400

    Tweak cke toolbars.

--- src/Sigil/Resource_Files/javascript/ckeditor/config.js
+++ src/Sigil/Resource_Files/javascript/ckeditor/config.js
@@ -1,15 +1,17 @@
-﻿CKEDITOR.editorConfig = function( config )
+﻿CKEDITOR.editorConfig = function(config)
 {
-	config.toolbar = 'Full';
-	config.toolbar_Full =
-	[
-	    
-	    { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord' ] },
-	    { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
-    	{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv', '-', 'JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
-    	{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
-    	{ name: 'insert', items : [ 'Table','HorizontalRule','SpecialChar','PageBreak' ] },
-    	{ name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
-    	{ name: 'colors', items : [ 'TextColor','BGColor' ] },
-	]
+    config.toolbarCanCollapse = false;
+    config.toolbar = 'Full';
+    config.toolbar_Full =
+    [
+        { name: 'clipboard', items : [ 'PasteText','PasteFromWord' ] },
+        { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+        { name: 'format', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv', ] },
+        { name: 'align', items : ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock' ] },
+        { name: 'direction', items : [ 'BidiLtr','BidiRtl' ] },
+        { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+        { name: 'insert', items : [ 'Table','HorizontalRule','SpecialChar','PageBreak' ] },
+        { name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
+        { name: 'colors', items : [ 'TextColor','BGColor' ] },
+    ]
 };
