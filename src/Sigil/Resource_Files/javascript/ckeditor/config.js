61e55598e3e69f7e5a939dc05dd70bea3753907f
John Schember <john@nachtimwald.com>
2012-05-04 22:30:19 -0400

    CKE: set doc type. Set XHTML valid equivelents for deprecated tags.

--- src/Sigil/Resource_Files/javascript/ckeditor/config.js
+++ src/Sigil/Resource_Files/javascript/ckeditor/config.js
@@ -15,4 +15,5 @@
         { name: 'colors', items : [ 'TextColor','BGColor' ] },
     ];
     config.entities = false;
+    config.docType = '&lt;!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"&gt;';
 };
