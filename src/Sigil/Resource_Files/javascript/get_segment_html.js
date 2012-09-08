6aa2421eea2e713049163e681e916964445157b5
Grant Drake <grant.drake@gmail.com>
2012-09-08 13:52:39 +0100

    Further for issue 1563: In Book View split at the current caret position rather than after the currently selected block, to prevent problems of not splitting correctly where block uses <br/> tags. Also prevents problem of leaving an empty <p></p> at the top of the split page.

--- src/Sigil/Resource_Files/javascript/get_segment_html.js
+++ src/Sigil/Resource_Files/javascript/get_segment_html.js
@@ -1,16 +1,59 @@
 // uses external get_block.js
 
 var node = document.getSelection().anchorNode;
-var caret_node = get_block( node );
+var offset = document.getSelection().anchorOffset;
+var splitNode = node.parentNode;
+var splitOffset = 0;
+
+if ( node.nodeType == 3 ) {
+    // We are in a #text node. will want to split it at the caret position
+    // if we are not at the end of the node text.
+    if ( offset <= node.length - 1 ) {
+        node.splitText( offset );
+        // Our range will be up to past this #text node
+        for ( var childIndex = 0, e = node; e = e.previousSibling; ++childIndex );
+        splitOffset = childIndex + 1;
+    }
+    else {
+        // The selection should be treated as past this node.
+        // We will walk up the hierarchy until either we find that:
+        // (a) we are not the last child node of this parent, or
+        // (b) we have reached the block node, and will split after that instead
+        var blockNode = get_block( node );
+        var parent = node.parentNode;
+        while ( parent != null ) {
+            if ( parent.childNodes[parent.childNodes.length - 1] != node ) {
+                // We are not the last parent so will split before the next node
+                for ( var childIndex = 0, e = node; e = e.previousSibling; ++childIndex );
+                splitNode = parent;
+                splitOffset = childIndex + 1;
+                break;
+            }
+            if ( parent == blockNode ) {
+                // We reached the parent and must be the last node within it
+                splitNode = parent.parentNode;
+                for ( var childIndex = 0, e = parent; e = e.previousSibling; ++childIndex );
+                splitOffset = childIndex + 1;
+                break;
+            }
+            node = parent;
+            parent = node.parentNode;
+        }
+    }
+}
+else {
+    // We are not in a #text node (such as between <br/> tags)
+    splitNode = node;
+    splitOffset = offset;
+}
 
 var range = document.createRange();
 range.setStart( document.body, 0 );
-range.setEnd( caret_node, caret_node.childNodes.length );  
+range.setEnd( splitNode, splitOffset );
 
 var serialiser = new XMLSerializer();
 var contentForFirstFile = new String( serialiser.serializeToString( range.extractContents() ) );
 
-// Explicitly remove the node as it may contain attrbutes that block this happening automatically
-caret_node.parentNode.removeChild( caret_node );
+var extractedText = contentForFirstFile.valueOf();
 
 contentForFirstFile.valueOf();
