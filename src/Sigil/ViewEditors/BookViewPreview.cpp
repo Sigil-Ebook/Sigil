1020a6421430a97b930fc0249f1e336434d40c39
Dave Heiland <dave.heiland@gmail.com>
2012-09-20 20:00:42 +0100

    Fix warning about variable order.

--- src/Sigil/ViewEditors/BookViewPreview.cpp
+++ src/Sigil/ViewEditors/BookViewPreview.cpp
@@ -46,8 +46,8 @@ const QString SET_CURSOR_JS =
 
 BookViewPreview::BookViewPreview(QWidget *parent)
     : QWebView(parent),
-      m_ViewWebPage(new ViewWebPage(this)),
       m_isLoadFinished(false),
+      m_ViewWebPage(new ViewWebPage(this)),
       c_jQuery(           Utility::ReadUnicodeTextFile( ":/javascript/jquery-1.6.2.min.js"           ) ),
       c_jQueryScrollTo(   Utility::ReadUnicodeTextFile( ":/javascript/jquery.scrollTo-1.4.2-min.js"  ) ),
       c_jQueryWrapSelection( Utility::ReadUnicodeTextFile( ":/javascript/jquery.wrapSelection.js"    ) ),
