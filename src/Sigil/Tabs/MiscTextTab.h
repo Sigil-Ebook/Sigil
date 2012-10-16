341c2bc84cb0bd76e6fe0a7c58a5891ada5e164e
Grant Drake <grant.drake@gmail.com>
2012-10-16 18:48:21 +0100

    Code cleanup of duplicate signal definition

--- src/Sigil/Tabs/MiscTextTab.h
+++ src/Sigil/Tabs/MiscTextTab.h
@@ -35,9 +35,6 @@ class MiscTextTab : public TextTab
 
 public:
     MiscTextTab( MiscTextResource &resource, int line_to_scroll_to = -1, QWidget *parent = 0);
-
-signals:
-    void OpenClipEditorRequest(ClipEditorModel::clipEntry *clip);
 };
 
 #endif // MISCTEXTTAB_H
