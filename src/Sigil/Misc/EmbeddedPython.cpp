abfd9a0bc7d77562d3fda5be3ecb82806432b9f4
Kevin Hendricks <kevinhendricks@users.noreply.github.com>
2015-01-03 19:44:14 -0500

    Make internal routines private and expand locking for embedded python

--- src/Sigil/Misc/EmbeddedPython.cpp
+++ src/Sigil/Misc/EmbeddedPython.cpp
@@ -139,6 +139,7 @@ QString EmbeddedPython::embeddedRoot()
 
 bool EmbeddedPython::addToPythonSysPath(const QString& mpath)
 {
+    EmbeddedPython::m_mutex.lock();
     PyObject* sysPath    = NULL;
     PyObject* aPath = NULL;
     bool success = false;
@@ -153,163 +154,12 @@ bool EmbeddedPython::addToPythonSysPath(const QString& mpath)
         }
     }
     Py_XDECREF(aPath);
+    EmbeddedPython::m_mutex.unlock();
     return success;
 }
 
-// call recursively to allow populating QVariant lists and lists of lists
-QVariant EmbeddedPython::PyObjectToQVariant(PyObject* po)
-{
-    QVariant res = QVariant(QString());
-
-    if ((po) == NULL)
-        return res;
-
-    if (PyLong_Check(po)) {
-        res = QVariant(PyLong_AsLongLong(po));
-
-    } else if (PyFloat_Check(po)) {
-        res = QVariant(PyFloat_AsDouble(po));
-
-    } else if (PyBytes_Check(po)) {
-        res = QVariant(QByteArray(PyBytes_AsString(po)));
-
-    } else if (PyUnicode_Check(po)) {
-
-        int kind = PyUnicode_KIND(po);
-
-        if (PyUnicode_READY(po) != 0)
-            return res;
-
-        if (kind == PyUnicode_1BYTE_KIND) {
-            // latin 1 according to PEP 393
-            res = QVariant(QString::fromLatin1(reinterpret_cast<const char *>PyUnicode_1BYTE_DATA(po), -1));
-
-        } else if (kind == PyUnicode_2BYTE_KIND) {
-            res = QVariant(QString::fromUtf16(PyUnicode_2BYTE_DATA(po), -1));
-
-        } else if (kind == PyUnicode_4BYTE_KIND) {
-            // PyUnicode_4BYTE_KIND
-            res = QVariant(QString::fromUcs4(PyUnicode_4BYTE_DATA(po), -1));
-
-        } else {
-            // convert to utf8 since not a known
-            res = QVariant(QString::fromUtf8(PyUnicode_AsUTF8(po),-1));
-        }
-
-    } else if (PyTuple_Check(po)) {
-        QVariantList vlist;
-        int n = PyTuple_Size(po);
-        for (int i=0; i< n; i++) {
-            vlist.append(PyObjectToQVariant(PyTuple_GetItem(po,i)));
-        }
-        res = QVariant(vlist);
-
-    } else if (PyList_Check(po)) {
-        QVariantList vlist;
-        int n = PyList_Size(po);
-        for (int i=0; i< n; i++) {
-            vlist.append(PyObjectToQVariant(PyList_GetItem(po,i)));
-        }
-        res = QVariant(vlist);
-
-    } else {
-       // do nothing here to return null value
-    }
-    return res;
-}
-
-
-// call recursively to allow populating tuples/lists and lists of lists
-PyObject* EmbeddedPython::QVariantToPyObject(QVariant & v)
-{
-    PyObject* value = NULL;
-    bool      ok;
-    switch ((QMetaType::Type)v.type()) {
-        case QMetaType::Double:
-            value = Py_BuildValue("d", v.toDouble(&ok));
-            break;
-        case QMetaType::Float:
-            value = Py_BuildValue("f", v.toFloat(&ok));
-            break;
-        case QMetaType::Int:
-            value = Py_BuildValue("i", v.toInt(&ok));
-            break;
-        case QMetaType::UInt:
-            value = Py_BuildValue("I", v.toUInt(&ok));
-            break;
-        case QMetaType::LongLong:
-            value = Py_BuildValue("L", v.toLongLong(&ok));
-            break;
-        case QMetaType::ULongLong:
-            value = Py_BuildValue("K", v.toULongLong(&ok));
-            break;
-        case QMetaType::QString:
-            value = PyUnicode_FromKindAndData(PyUnicode_2BYTE_KIND, v.toString().utf16(), v.toString().size());
-            // value = Py_BuildValue("s", v.toString().toUtf8().constData());
-            break;
-        case QMetaType::QByteArray:
-            value = Py_BuildValue("y", v.toByteArray().constData());
-            break;
-        case QMetaType::QStringList:
-            // fall through
-        case QMetaType::QVariantList: {
-                QVariantList vlist = v.toList();
-                value = PyList_New(vlist.size());
-                int idx=0;
-                foreach(QVariant av, vlist) {
-                    PyList_SetItem(value, idx, QVariantToPyObject(av));
-                    idx++;
-                }
-            } break;
-        default:
-            // Ensure we don't have any holes.
-            value = Py_BuildValue("u", "");
-            break;
-        }
-    return value;
-}
-
-QString EmbeddedPython::getPythonErrorTraceback(bool useMsgBox)
-{
-    PyObject     *etype      = NULL;
-    PyObject     *evalue     = NULL;
-    PyObject     *etraceback = NULL;
-    PyObject     *mod        = NULL;
-    PyObject     *elist      = NULL;
-    QStringList  tblist;
-    
-    PyErr_Fetch(&etype, &evalue, &etraceback);
-    PyErr_NormalizeException(&etype, &evalue, &etraceback);
-
-    mod = PyImport_ImportModule("traceback");
-
-    if (mod) {
-        elist   = PyObject_CallMethod(mod, "format_exception", "OOO", etype, evalue, etraceback);
-        if (elist != NULL) {
-            tblist = PyObjectToQVariant(elist).toStringList();
-        } else {
-            tblist.append(QString("Error: traceback report is missing"));
-        }
-    } else {
-        tblist.append(QString("Error: traceback module failed to load"));
-    }
-
-    Py_XDECREF(elist);
-    Py_XDECREF(mod);
-    Py_XDECREF(etraceback);
-    Py_XDECREF(evalue);
-    Py_XDECREF(etype);
-
-    PyErr_Clear();
-
-    QString tb = tblist.join(QString("\n"));
-    if (useMsgBox) {
-        QString message = QString(tr("Embedded Python Error"));
-        Utility::DisplayStdErrorDialog(message, tb);
-    }
-    return tb;
-}
-
+// run interpreter without initiating/locking/unlocking GIL 
+// in a single thread at a time
 QVariant EmbeddedPython::runInPython(const QString& mname, 
                                      const QString& fname, 
                                      const QVariantList& args, 
@@ -378,4 +228,166 @@ cleanup:
 
     EmbeddedPython::m_mutex.unlock();
     return res;
+}
+
+
+// *** below here all routines are private and only invoked 
+// *** from runInPython with lock held
+
+
+// Convert PyObject types to their QVariant equivalents 
+// call recursively to allow populating QVariant lists and lists of lists
+QVariant EmbeddedPython::PyObjectToQVariant(PyObject* po)
+{
+    QVariant res = QVariant(QString());
+
+    if ((po) == NULL)
+        return res;
+
+    if (PyLong_Check(po)) {
+        res = QVariant(PyLong_AsLongLong(po));
+
+    } else if (PyFloat_Check(po)) {
+        res = QVariant(PyFloat_AsDouble(po));
+
+    } else if (PyBytes_Check(po)) {
+        res = QVariant(QByteArray(PyBytes_AsString(po)));
+
+    } else if (PyUnicode_Check(po)) {
+
+        int kind = PyUnicode_KIND(po);
+
+        if (PyUnicode_READY(po) != 0)
+            return res;
+
+        if (kind == PyUnicode_1BYTE_KIND) {
+            // latin 1 according to PEP 393
+            res = QVariant(QString::fromLatin1(reinterpret_cast<const char *>PyUnicode_1BYTE_DATA(po), -1));
+
+        } else if (kind == PyUnicode_2BYTE_KIND) {
+            res = QVariant(QString::fromUtf16(PyUnicode_2BYTE_DATA(po), -1));
+
+        } else if (kind == PyUnicode_4BYTE_KIND) {
+            // PyUnicode_4BYTE_KIND
+            res = QVariant(QString::fromUcs4(PyUnicode_4BYTE_DATA(po), -1));
+
+        } else {
+            // convert to utf8 since not a known
+            res = QVariant(QString::fromUtf8(PyUnicode_AsUTF8(po),-1));
+        }
+
+    } else if (PyTuple_Check(po)) {
+        QVariantList vlist;
+        int n = PyTuple_Size(po);
+        for (int i=0; i< n; i++) {
+            vlist.append(PyObjectToQVariant(PyTuple_GetItem(po,i)));
+        }
+        res = QVariant(vlist);
+
+    } else if (PyList_Check(po)) {
+        QVariantList vlist;
+        int n = PyList_Size(po);
+        for (int i=0; i< n; i++) {
+            vlist.append(PyObjectToQVariant(PyList_GetItem(po,i)));
+        }
+        res = QVariant(vlist);
+
+    } else {
+       // do nothing here to return null value
+    }
+    return res;
+}
+
+// Convert QVariant to a Python Equivalent Type
+// call recursively to allow populating tuples/lists and lists of lists
+PyObject* EmbeddedPython::QVariantToPyObject(QVariant & v)
+{
+    PyObject* value = NULL;
+    bool      ok;
+    switch ((QMetaType::Type)v.type()) {
+        case QMetaType::Double:
+            value = Py_BuildValue("d", v.toDouble(&ok));
+            break;
+        case QMetaType::Float:
+            value = Py_BuildValue("f", v.toFloat(&ok));
+            break;
+        case QMetaType::Int:
+            value = Py_BuildValue("i", v.toInt(&ok));
+            break;
+        case QMetaType::UInt:
+            value = Py_BuildValue("I", v.toUInt(&ok));
+            break;
+        case QMetaType::LongLong:
+            value = Py_BuildValue("L", v.toLongLong(&ok));
+            break;
+        case QMetaType::ULongLong:
+            value = Py_BuildValue("K", v.toULongLong(&ok));
+            break;
+        case QMetaType::QString:
+            value = PyUnicode_FromKindAndData(PyUnicode_2BYTE_KIND, v.toString().utf16(), v.toString().size());
+            // value = Py_BuildValue("s", v.toString().toUtf8().constData());
+            break;
+        case QMetaType::QByteArray:
+            value = Py_BuildValue("y", v.toByteArray().constData());
+            break;
+        case QMetaType::QStringList:
+            // fall through
+        case QMetaType::QVariantList: {
+                QVariantList vlist = v.toList();
+                value = PyList_New(vlist.size());
+                int idx=0;
+                foreach(QVariant av, vlist) {
+                    PyList_SetItem(value, idx, QVariantToPyObject(av));
+                    idx++;
+                }
+            } break;
+        default:
+            // Ensure we don't have any holes.
+            value = Py_BuildValue("u", "");
+            break;
+        }
+    return value;
+}
+
+
+// get traceback from inside interpreter upon error
+QString EmbeddedPython::getPythonErrorTraceback(bool useMsgBox)
+{
+    PyObject     *etype      = NULL;
+    PyObject     *evalue     = NULL;
+    PyObject     *etraceback = NULL;
+    PyObject     *mod        = NULL;
+    PyObject     *elist      = NULL;
+    QStringList  tblist;
+    
+    PyErr_Fetch(&etype, &evalue, &etraceback);
+    PyErr_NormalizeException(&etype, &evalue, &etraceback);
+
+    mod = PyImport_ImportModule("traceback");
+
+    if (mod) {
+        elist   = PyObject_CallMethod(mod, "format_exception", "OOO", etype, evalue, etraceback);
+        if (elist != NULL) {
+            tblist = PyObjectToQVariant(elist).toStringList();
+        } else {
+            tblist.append(QString("Error: traceback report is missing"));
+        }
+    } else {
+        tblist.append(QString("Error: traceback module failed to load"));
+    }
+
+    Py_XDECREF(elist);
+    Py_XDECREF(mod);
+    Py_XDECREF(etraceback);
+    Py_XDECREF(evalue);
+    Py_XDECREF(etype);
+
+    PyErr_Clear();
+
+    QString tb = tblist.join(QString("\n"));
+    if (useMsgBox) {
+        QString message = QString(tr("Embedded Python Error"));
+        Utility::DisplayStdErrorDialog(message, tb);
+    }
+    return tb;
 }
