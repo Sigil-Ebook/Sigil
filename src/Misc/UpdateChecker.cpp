0eb698b664f973b9ee377596608866ff34663c0d
John Schember <john@nachtimwald.com>
2015-06-14 21:23:52 -0400

    Update links.

--- src/Misc/UpdateChecker.cpp
+++ src/Misc/UpdateChecker.cpp
@@ -35,7 +35,7 @@
 #include "sigil_constants.h"
 
 static const QString DOWNLOAD_PAGE_LOCATION  = "http://sigil-ebook.com/get";
-static const QString UPDATE_XML_LOCATION     = "https://github.com/user-none/Sigil/raw/master/version.xml";
+static const QString UPDATE_XML_LOCATION     = "https://github.com/Sigil-Ebook/Sigil/raw/master/version.xml";
 static const QString XML_VERSION_ELEMENT     = "current-version";
 static const QString LAST_ONLINE_VERSION_KEY = "last_online_version";
 static const QString LAST_CHECK_TIME_KEY     = "last_check_time";
