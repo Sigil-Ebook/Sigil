ed51d4f56034c963891aa46907025b93f1194e3a
Kevin Hendricks <kevinhendricks@users.noreply.github.com>
2015-09-02 17:06:23 -0400

    Remove leftover code snippet inadvertently committed

--- src/Misc/piece.h
+++
@@ -1,6 +0,0 @@
-    QString prettyprint(QString indent_chars="  ");
-
-private:
-
-    std::string prettyprint(GumboNode* node, int lvl, const std::string indent_chars);
-    std::string prettyprint_contents(GumboNode* node, int lvl, const std::string indent_chars);
