94548bac5a0da365e7f3c5bbdee5990696248574
Kevin Hendricks <kevinhendricks@users.noreply.github.com>
2015-09-24 17:23:52 -0400

    Fix self generated deadlock when Adding a Cover image

--- src/ResourceObjects/OPFResource.cpp
+++ src/ResourceObjects/OPFResource.cpp
@@ -347,7 +347,8 @@ void OPFResource::AddSigilVersionMeta()
 
 bool OPFResource::IsCoverImage(const ImageResource *image_resource) const
 {
-    QReadLocker locker(&GetLock());
+    // locking here causes a deadlock since SetResourceAsCover grabs a writelock 
+    // QReadLocker locker(&GetLock());
     QString source = CleanSource::ProcessXML(GetText());
     OPFParser p;
     p.parse(source);
