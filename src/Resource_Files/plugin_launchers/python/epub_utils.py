424678f9cad39ccd90006f6bbc356aa6163942ce
Kevin Hendricks <kevinhendricks@users.noreply.github.com>
2016-01-07 11:15:41 -0500

    Fix bugs in font handling in epub_utils and add Idpf mangling code

--- src/Resource_Files/plugin_launchers/python/epub_utils.py
+++ src/Resource_Files/plugin_launchers/python/epub_utils.py
@@ -26,12 +26,12 @@
 # WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from __future__ import unicode_literals, division, absolute_import, print_function
-from compatibility_utils import text_type, binary_type, utf8_str, unicode_str, bord, bchr
 
 import sys, os
 import struct, array, binascii, re
-
 from itertools import cycle
+
+from compatibility_utils import text_type, binary_type, utf8_str, unicode_str, bord, bchr
 
 import zlib
 import zipfile
@@ -65,7 +65,6 @@ def unzip_epub_to_dir(path_to_epub, destdir):
     f.close()
 
 
-
 def epub_zip_up_book_contents(ebook_path, epub_filepath):
     outzip = zipfile.ZipFile(pathof(epub_filepath), 'w')
     files = unipath.walk(ebook_path)
@@ -80,7 +79,6 @@ def epub_zip_up_book_contents(ebook_path, epub_filepath):
     outzip.close()
 
 
-
 def build_container_xml(relative_path_to_opf):
     opf_path = unicode_str(relative_path_to_opf)
     container = '<?xml version="1.0" encoding="UTF-8"?>\n'
@@ -89,7 +87,6 @@ def build_container_xml(relative_path_to_opf):
     container += '<rootfile full-path="%s" media-type="application/oebps-package+xml"/>' % opf_path
     container += '    </rootfiles>\n</container>\n'
     return container
-
 
 
 def build_adobe_encryption_xml(mangled_fonts):
@@ -101,14 +98,31 @@ def build_adobe_encryption_xml(mangled_fonts):
             encryption += '  <enc:EncryptedData>\n'
             encryption += '    <enc:EncryptionMethod Algorithm="http://ns.adobe.com/pdf/enc#RC"/>\n'
             encryption += '    <enc:CipherData>\n'
-            encryption += '      <enc:CipherReference URI="OEBPS/Fonts/' + font + '"/>\n' % fontfile
+            encryption += '      <enc:CipherReference URI="OEBPS/Fonts/' + fontfile + '"/>\n'
             encryption += '    </enc:CipherData>\n'
             encryption += '  </enc:EncryptedData>\n'
         encryption += '</encryption>\n'
     return encryption
 
 
+# mangeled_fonts is a simple list of Font file names (no href or path needed)
+def build_idpf_encryption_xml(mangled_fonts):
+    encryption = ''
+    if mangled_fonts is not None and len(mangled_fonts) > 0:
+        encryption = '<encryption xmlns="urn:oasis:names:tc:opendocument:xmlns:container" \
+            xmlns:enc="http://www.w3.org/2001/04/xmlenc#">\n'
+        for fontfile in mangled_fonts:
+            encryption += '  <enc:EncryptedData>\n'
+            encryption += '    <enc:EncryptionMethod Algorithm="http://www.idpf.org/2008/embedding"/>\n'
+            encryption += '    <enc:CipherData>\n'
+            encryption += '      <enc:CipherReference URI="OEBPS/Fonts/' + fontfile + '"/>\n'
+            encryption += '    </enc:CipherData>\n'
+            encryption += '  </enc:EncryptedData>\n'
+        encryption += '</encryption>\n'
+    return encryption
 
+
+# mangeled_fonts is a simple list of Font file names (no href or path needed)
 def Adobe_encryption_key(uid):
     # strip it down to simple valid hex characters
     # being careful to generate a string of 16 bytes in length
@@ -121,17 +135,17 @@ def Adobe_encryption_key(uid):
     return key
 
 
-
 def Idpf_encryption_key(uid):
     # remove whitespace changing nothing else
     key = utf8_str(uid)
-    key = key.replace(bchr(x20),b'')
-    key = key.replace(bchr(x09),b'')
-    key = key.replace(bchr(x0d),b'')
-    key = key.replace(bchr(x0a),b'')
+    if key.startswith(b"urn:uuid:"):
+        key = key[9:]
+    key = key.replace(bchr(0x20),b'')
+    key = key.replace(bchr(0x09),b'')
+    key = key.replace(bchr(0x0d),b'')
+    key = key.replace(bchr(0x0a),b'')
     key = SHA1(key)
     return key
-
 
 
 def Adobe_mangle_fonts(encryption_key, data):
@@ -141,7 +155,6 @@ def Adobe_mangle_fonts(encryption_key, data):
     key = cycle(iter(map(bord, encryption_key)))
     encrypt = b''.join([bchr(bord(x)^next(key)) for x in crypt])
     return encrypt + data[1024:]
-
 
 
 def Idpf_mangle_fonts(encryption_key, data):
