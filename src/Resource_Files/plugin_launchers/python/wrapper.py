e1afcaa535c4a26b453bdc1c598cf55b81ec93ad
Kevin Hendricks <kevinhendricks@users.noreply.github.com>
2016-01-30 10:38:04 -0500

    Updating launcher version to 20160130 for upcoming release

--- src/Resource_Files/plugin_launchers/python/wrapper.py
+++ src/Resource_Files/plugin_launchers/python/wrapper.py
@@ -35,7 +35,7 @@ import unipath
 from unipath import pathof
 import unicodedata
 
-_launcher_version=20160113
+_launcher_version=20160130
 
 _PKG_VER = re.compile(r'''<\s*package[^>]*version\s*=\s*["']([^'"]*)['"][^>]*>''',re.IGNORECASE)
 
