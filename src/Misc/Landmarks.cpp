d4e43a70ed9c9f4d926730dab6b1a51fb2cdd155
Kevin Hendricks <kevinhendricks@users.noreply.github.com>
2016-02-29 16:31:07 -0500

    Restrict all semantics to those for section, body, and sectioning contents for Landmarks

--- src/Misc/Landmarks.cpp
+++ src/Misc/Landmarks.cpp
@@ -109,12 +109,16 @@ void Landmarks::SetLandmarksMap()
         return;
     }
 
-    //  See http://www.idpf.org/epub/vocab/structure/
+    // See http://www.idpf.org/epub/vocab/structure/ (you need to look at previous versions to find the one
+    // specific to the epub 3.0.1 spec
+
     QStringList data;
     data <<
         tr("Acknowledgments") << "acknowledgments" << tr("A passage containing acknowledgments to entities involved in the realization of the work.") <<
         tr("Afterword") << "afterword" << tr("A closing statement from the author or a person of importance to the story, typically providing insight into how the story came to be written, its significance or related events that have transpired since its timeline.") <<
+        tr("Annotation") << "annotation" << tr("Explanatory information about passages in the work. Status: Deprecated") <<
         tr("Appendix") << "appendix" << tr("Supplemental information.") <<
+        tr("Assessment") << "assessment" << tr("A test, quiz, or other activity that helps measure a student's understanding of what is being taught.") <<
         tr("Back Matter") << "backmatter" << tr("Ancillary material occurring after the main content of a publication, such as indices, appendices, etc.") <<
         tr("Bibliography") << "bibliography" << tr("A list of works cited.") <<
         tr("Body Matter") << "bodymatter" << tr("The main content of a publication.") <<
@@ -123,7 +127,7 @@ void Landmarks::SetLandmarksMap()
         tr("Conclusion") << "conclusion" << tr("An ending section that typically wraps up the work.") <<
         tr("Contributors") << "contributors" << tr("A list of contributors to the work.") <<
         tr("Copyright Page") << "copyright-page" << tr("The copyright page of the work.") <<
-        tr("Cover Page") << "cover" << tr("The publications cover(s), jacket information, etc.") <<
+        tr("Cover") << "cover" << tr("The publications cover(s), jacket information, etc.") <<
         tr("Dedication") << "dedication" << tr("An inscription addressed to one or several particular person(s).") <<
         tr("Division") << "division" << tr("A major structural division that may also appear as a substructure of a part (esp. in legislation).") <<
         tr("Epigraph") << "epigraph" << tr("A quotation that is pertinent but not integral to the text.") <<
@@ -133,9 +137,9 @@ void Landmarks::SetLandmarksMap()
         tr("Foreword") << "foreword" << tr("An introductory section that precedes the work, typically not written by the work's author.") <<
         tr("Front Matter") << "frontmatter" << tr("Preliminary material to the main content of a publication, such as tables of contents, dedications, etc.") <<
         tr("Glossary") << "glossary" << tr("An alphabetical list of terms in a particular domain of knowledge, with the definitions for those terms.") <<
-        tr("Half Ttitle Page") << "halftitlepage" << tr("The half title page of the work which carries just the title itself.") <<
-        tr("Imprint") << "imprint" << tr("Information relating to the publication or distribution of the work.") <<
+        tr("Half Title Page") << "halftitlepage" << tr("The half title page of the work which carries just the title itself.") <<
         tr("Imprimatur") << "imprimatur" << tr("A formal statement authorizing the publication of the work.") <<
+        tr("Imprint") << "imprint" << tr("Information relating to the publication or distribution of the work.") <<
         tr("Index") << "index" << tr("A detailed list, usually arranged alphabetically, of the specific information in a publication.") <<
         tr("Introduction") << "introduction" << tr("A section in the beginning of the work, typically introducing the reader to the scope or nature of the work's content.") <<
         tr("Landmarks") << "landmarks" << tr("A collection of references to well-known/recurring components within the publication") <<
@@ -143,26 +147,21 @@ void Landmarks::SetLandmarksMap()
         tr("List of Illustrations") << "loi" << tr("A listing of illustrations included in the work.") <<
         tr("List of Tables") << "lot" << tr("A listing of tables included in the work.") <<
         tr("List of Video Clips") << "lov" << tr("A listing of video clips included in the work.") <<
-        tr("List of Pages") << "page-list" << tr("A list of links to page start locations from a print version of the ebook") <<
         tr("Notice") << "notice" << tr("Information that requires special attention, and that must not be skipped or suppressed. Examples include: alert, warning, caution, danger, important.") <<
         tr("Other Credits") << "other-credits" << tr("Acknowledgments of previously published parts of the work, illustration credits, and permission to quote from copyrighted material.") <<
+        tr("Page List") << "page-list" << tr("A list of references to pagebreaks (start locations) from a print version of the ebook") <<
         tr("Part") << "part" << tr("A major structural division of a piece of writing, typically encapsulating a set of related chapters.") <<
         tr("Preamble") << "preamble" << tr("A section in the beginning of the work, typically containing introductory and/or explanatory prose regarding the scope or nature of the work's content") <<
         tr("Preface") << "preface" << tr("An introductory section that precedes the work, typically written by the work's author.") <<
         tr("Prologue") << "prologue" << tr("An introductory section that sets the background to a story, typically part of the narrative.") <<
+        tr("Questions and Answers") << "qna" << tr("A question and answer section.") <<
         tr("Rear Notes") << "rearnotes" << tr("A collection of notes appearing at the rear (backmatter) of the work, or at the end of a section.") <<
+        tr("Revision History") << "revision-history" << tr("A record of changes made to a work.") <<
         tr("Subchapter") << "subchapter" << tr("A major sub-division of a chapter.") <<
         tr("Title Page") << "titlepage" << tr("A page at the beginning of a book giving its title, authors, publisher and other publication information.") <<
         tr("Table of Contents") << "toc" << tr("A table of contents which is a list of the headings or parts of the book or document, organized in the order in which they appear. Typically appearing in the work's frontmatter, or at the beginning of a section.") <<
         tr("Volume") << "volume" << tr("A component of a collection.") <<
-        tr("Warning") << "warning" << tr("A warning or caution about specific material. Status: Deprecated - Replaced by 'notice'.") <<
-        tr("Abstract") << "abstract" << tr("A short summary of the principle ideas, concepts and conclusions of the work, or of a section or except within it. Status: Draft.") <<
-        tr("Annotation") << "annotation" << tr("Explanatory information about passages in the work. Status: Deprecated") <<
-        tr("Keywords") << "keywords" << tr("A collection of key words or phrases used to previde searchable metadata. Status: Draft") <<
-        tr("Notes") << "notes" << tr("A collection of notes. It can be used to identify footnotes, rear notes, marginal notes, inline notes, and similar when legacy naming conventions are not desired. Status: Deprecated - Replaced by: 'footnotes', 'rearnotes'") <<
-        tr("Revision History") << "revision-history" << tr("A record of changes made to a work.") <<
-        tr("Series Page") << "seriespage" << tr("Marketing section used to list related publications. Status: Draft") <<
-        tr("Sidebar") << "sidebar" << tr("Secondary or supplementary content, typically formatted as an inset or box.");
+        tr("Warning") << "warning" << tr("A warning or caution about specific material. Status: Deprecated - Replaced by 'notice'.");
 
 
     for (int i = 0; i < data.count(); i++) {
