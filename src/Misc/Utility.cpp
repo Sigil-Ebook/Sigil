b6e3570623e01156b0697fb95aa3fb85d9920b43
Kevin Hendricks <kevinhendricks@users.noreply.github.com>
2017-11-14 18:30:51 -0500

    Fix smart apostrophe spellcheck issue specific to Windows

--- src/Misc/Utility.cpp
+++ src/Misc/Utility.cpp
@@ -627,10 +627,10 @@ QString Utility::getSpellingSafeText(const QString &raw_text)
 {
     // There is currently a problem with Hunspell if we attempt to pass
     // words with smart apostrophes from the CodeView encoding.
-    // There are likely better ways to solve this, but this one does
-    // get the job done until someone can implement something better.
+    // Hunspell dictionaries typically store their main wordlist using
+    // the dumb apostrophe variants only to save space and speed checking
     QString text(raw_text);
-    return text.replace(QString::fromUtf8("\u2019"), "'");
+    return text.replace(QChar(0x2019),QChar(0x27));
 }
 
 
