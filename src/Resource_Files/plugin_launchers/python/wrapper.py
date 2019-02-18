2c70d2e51b937e4aa51bc6748634353c4b4ad3b4
Doug Massay <douglaslmassay@gmail.com>
2019-02-18 16:53:28 -0500

    Tweak changelog update launcher version

--- src/Resource_Files/plugin_launchers/python/wrapper.py
+++ src/Resource_Files/plugin_launchers/python/wrapper.py
@@ -35,7 +35,7 @@ import unipath
 from unipath import pathof
 import unicodedata
 
-_launcher_version=20190214
+_launcher_version=20190218
 
 _PKG_VER = re.compile(r'''<\s*package[^>]*version\s*=\s*["']([^'"]*)['"][^>]*>''',re.IGNORECASE)
 
