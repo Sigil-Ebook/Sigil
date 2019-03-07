de7b09f3b693c584f70cb22d1b68203e017874c3
Doug Massay <douglaslmassay@gmail.com>
2019-03-07 07:09:40 -0500

    Update url attributes in all shape-outside properties in CSS stylesheets

--- src/SourceUpdates/PerformCSSUpdates.cpp
+++ src/SourceUpdates/PerformCSSUpdates.cpp
@@ -50,7 +50,7 @@ QString PerformCSSUpdates::operator()()
 
     // Now parse the text once looking for keys and replacing them where needed
     QRegularExpression reference(
-        "(?:(?:src|background|background-image|list-style|list-style-image|border-image|border-image-source|content)\\s*:|@import)\\s*"
+        "(?:(?:src|background|background-image|list-style|list-style-image|border-image|border-image-source|content|(?:-webkit-)?shape-outside)\\s*:|@import)\\s*"
         "("
         "[^;\\}]*"
         ")"
