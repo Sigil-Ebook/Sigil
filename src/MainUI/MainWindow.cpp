070bb3900d5e93d5ea2c3ad0fc314035b893c3ac
Kevin Hendricks <kevin.b.hendricks@icloud.com>
2019-03-12 08:41:29 -0400

    macOS only clear memory caches when closing a MainWindow

--- src/MainUI/MainWindow.cpp
+++ src/MainUI/MainWindow.cpp
@@ -224,6 +224,7 @@ MainWindow::~MainWindow()
     }
 
 #ifdef Q_OS_MAC
+    QWebSettings::clearMemoryCaches();
     if (m_ClipboardHistorySelector) delete m_ClipboardHistorySelector;
     if (m_LinkOrStyleBookmark) delete m_LinkOrStyleBookmark;
     if (m_Reports) delete m_Reports;
