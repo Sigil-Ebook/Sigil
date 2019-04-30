09874947e168f0727e3e08b1439e781a7a621f59
Doug Massay <douglaslmassay@gmail.com>
2019-04-29 20:04:34 -0400

    Unambiguously specifiy Sigil's desktop filename

--- src/main.cpp
+++ src/main.cpp
@@ -322,6 +322,10 @@ int main(int argc, char *argv[])
         // and on Mac by the ICNS file.
 #if !defined(Q_OS_WIN32) && !defined(Q_OS_MAC)
         app.setWindowIcon(GetApplicationIcon());
+#if QT_VERSION >= 0x050700
+        // Wayland needs this clarified in order to propery assign the icon 
+        app.setDesktopFileName(QStringLiteral("sigil.desktop"));
+#endif
 #endif
         // Needs to be created on the heap so that
         // the reply has time to return.
