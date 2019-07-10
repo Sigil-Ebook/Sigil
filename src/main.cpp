ba41ceb7428c9ab34e5ed58201b61c35200c0197
Kevin Hendricks <kevin.b.hendricks@icloud.com>
2019-07-10 10:40:36 -0400

    fix typo and force travis rebuild with new Python

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /************************************************************************
 **
-**  Copyright (C) 2018-2019  Kevin B. Hendricks, Stratford, Ontario, Canada
+**  Copyright (C) 2018-2019  Kevin B. Hendricks, Stratford, Ontario Canada
 **  Copyright (C) 2009, 2010, 2011  Strahinja Markovic  <strahinja.markovic@gmail.com>
 **
 **  This file is part of Sigil.
