6e9a0056170fbf87a13460fdd10bfa80bd8291c7
Kevin Hendricks <kevin.b.hendricks@icloud.com>
2019-07-16 09:36:27 -0400

    bump versions for future release

--- src/Resource_Files/plugin_launchers/python/wrapper.py
+++ src/Resource_Files/plugin_launchers/python/wrapper.py
@@ -35,7 +35,7 @@ import unipath
 from unipath import pathof
 import unicodedata
 
-_launcher_version=20190714
+_launcher_version=20190716
 
 _PKG_VER = re.compile(r'''<\s*package[^>]*version\s*=\s*["']([^'"]*)['"][^>]*>''',re.IGNORECASE)
 
