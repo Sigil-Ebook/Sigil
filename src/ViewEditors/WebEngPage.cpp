a2f334ee5e96b1ea270c35b7ff8d2d6cbfcf5d47
kevinhendricks <kevin.b.hendricks@icloud.com>
2019-09-04 12:12:54 -0400

    intercept javascript location.href style links as NavigationTypeOther

--- src/ViewEditors/WebEngPage.cpp
+++ src/ViewEditors/WebEngPage.cpp
@@ -47,13 +47,13 @@ WebEngPage::WebEngPage(QObject *parent)
 
 bool WebEngPage::acceptNavigationRequest(const QUrl & url, QWebEnginePage::NavigationType type, bool isMainFrame)
 {
-    if (type == QWebEnginePage::NavigationTypeLinkClicked) {
+    if ((type == QWebEnginePage::NavigationTypeLinkClicked) || (type == QWebEnginePage::NavigationTypeOther)) {
         DBG qDebug() << "acceptNavigationRequest " << url.toString() << " , " << type << " , " << isMainFrame;
         m_url = url;
 	QTimer::singleShot(20,this,SLOT(EmitLinkClicked()));
         return false;
     }
-    DBG qDebug() << " at end of acceptNavigationRequest with type: " << type;
+    DBG qDebug() << " Unhandled acceptNavigationRequest with type: " << type;
     return true;
 }
 
