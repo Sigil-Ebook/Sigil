8f9054afc3849c71e4b67fedee8a2fa900bec47a
kevinhendricks <kevin.b.hendricks@icloud.com>
2019-11-10 14:53:38 -0500

    fix mistake in copyright header updates

--- src/Tabs/XMLTab.h
+++ src/Tabs/XMLTab.h
@@ -1,6 +1,6 @@
 /************************************************************************
 **
-**  Copyright (C) 2015-2019 Strahinja Markovic  <strahinja.markovic@gmail.com>
+**  Copyright (C) 2015-2019 Kevin B. Hendricks, Stratford Ontario Canada
 **  Copyright (C) 2009-2011 Strahinja Markovic  <strahinja.markovic@gmail.com>
 **
 **  This file is part of Sigil.
