403343009fdbd49bff48ec29e3458c5f1510f8ed
Doug Massay <douglaslmassay@gmail.com>
2020-01-25 11:21:49 -0500

    Tweak darkmode text to just offwhite

--- src/main.cpp
+++ src/main.cpp
@@ -283,20 +283,20 @@ QPalette getDarkPalette()
 
     darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
     darkPalette.setColor(QPalette::Disabled, QPalette::Window, QColor(80, 80, 80));
-    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::WindowText, QColor(238, 238, 238));
     darkPalette.setColor(QPalette::Disabled, QPalette::WindowText,
                         QColor(127, 127, 127));
     darkPalette.setColor(QPalette::Base, QColor(42, 42, 42));
     darkPalette.setColor(QPalette::Disabled, QPalette::Base, QColor(80, 80, 80));
     darkPalette.setColor(QPalette::AlternateBase, QColor(66, 66, 66));
     darkPalette.setColor(QPalette::ToolTipBase, QColor(53, 53, 53));
-    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
-    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, QColor(238, 238, 238));
+    darkPalette.setColor(QPalette::Text, QColor(238, 238, 238));
     darkPalette.setColor(QPalette::Disabled, QPalette::Text, QColor(127, 127, 127));
     darkPalette.setColor(QPalette::Dark, QColor(35, 35, 35));
     darkPalette.setColor(QPalette::Shadow, QColor(20, 20, 20));
     darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
-    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::ButtonText, QColor(238, 238, 238));
     darkPalette.setColor(QPalette::Disabled, QPalette::ButtonText,
                         QColor(127, 127, 127));
     darkPalette.setColor(QPalette::BrightText, Qt::red);
@@ -304,7 +304,7 @@ QPalette getDarkPalette()
     darkPalette.setColor(QPalette::LinkVisited, QColor(108, 180, 238));
     darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
     darkPalette.setColor(QPalette::Disabled, QPalette::Highlight, QColor(80, 80, 80));
-    darkPalette.setColor(QPalette::HighlightedText, Qt::white);
+    darkPalette.setColor(QPalette::HighlightedText, QColor(238, 238, 238));
     darkPalette.setColor(QPalette::Disabled, QPalette::HighlightedText,
                         QColor(127, 127, 127));
 
