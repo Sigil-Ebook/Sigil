f78c07160c4b84731c4cc5e311a4aee50ce7cb39
Kevin Hendricks <kevinhendricks@users.noreply.github.com>
2020-11-14 19:51:14 -0500

    revert making ctrl-f a toogle

--- src/MainUI/MainWindow.cpp
+++ src/MainUI/MainWindow.cpp
@@ -1652,10 +1652,6 @@ void MainWindow::Exit()
 
 void MainWindow::Find()
 {
-    if (m_FindReplace->isVisible()) {
-        m_FindReplace->hide();
-        return;
-    }
     SaveTabData();
     m_FindReplace->SetUpFindText();
     m_FindReplace->show();
