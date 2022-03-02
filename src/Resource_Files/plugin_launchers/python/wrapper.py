15f7ef92d64512235a80363b1d44c06b8ef641a3
Kevin Hendricks <kevinhendricks@users.noreply.github.com>
2022-03-02 11:06:52 -0500

    update versions for follow-up release

--- src/Resource_Files/plugin_launchers/python/wrapper.py
+++ src/Resource_Files/plugin_launchers/python/wrapper.py
@@ -49,7 +49,7 @@ def _unicodestr(p):
         return p
     return p.decode('utf-8', errors='replace')
 
-_launcher_version = 20220101
+_launcher_version = 20220302
 
 _PKG_VER = re.compile(r'''<\s*package[^>]*version\s*=\s*["']([^'"]*)['"][^>]*>''', re.IGNORECASE)
 
