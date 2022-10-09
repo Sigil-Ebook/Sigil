e7c194fe821eaf6315e7529c7e5c23f365073adb
Doug Massay <douglaslmassay@gmail.com>
2022-10-09 11:05:52 -0400

    Add Kevin's function to change Checkpoint msgs [skip ci]

--- src/Resource_Files/python3lib/repomanager.py
+++ src/Resource_Files/python3lib/repomanager.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 # vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
 
-# Copyright (c) 2020 Kevin B. Hendricks, Stratford Ontario Canada
+# Copyright (c) 2022 Kevin B. Hendricks, Stratford Ontario Canada
+#               2022 Doug Massay
 # All rights reserved.
 #
 # This file is part of Sigil.
@@ -43,7 +44,7 @@ if (sys.hexversion < 0x03080000) and not hasattr(sys, 'argv'):
 import dulwich
 from dulwich import porcelain
 from dulwich.repo import Repo
-from dulwich.porcelain import open_repo_closing, show_object, print_commit, commit_decode
+from dulwich.porcelain import open_repo_closing, show_object, print_commit, commit_decode, _make_tag_ref
 from dulwich.objects import Tag, Commit, Blob, check_hexsha, ShaFile, Tree, format_timezone
 from dulwich.refs import ANNOTATED_TAG_SUFFIX
 from dulwich.patch import write_tree_diff
@@ -715,6 +716,41 @@ def get_current_status_vs_destdir(bookroot, bookfiles, destdir):
     for i in range(len(modified)):
         modified[i] = modified[i].replace(os.sep,"/")
     return (deleted, added, modified)
+
+
+def update_annotated_tag_message(localRepo, bookid, tagname, newmessage):
+    repo_home = pathof(localRepo)
+    repo_home = repo_home.replace("/", os.sep)
+    repo_path = os.path.join(repo_home, "epub_" + bookid)
+    cdir = os.getcwd()
+    if os.path.exists(repo_path):
+        os.chdir(repo_path)
+        with open_repo_closing(".") as r:
+            tag_name = utf8_str(tagname)
+            tags = sorted(r.refs.as_dict(b"refs/tags"))
+            tagkey = b"refs/tags/" + tag_name
+            if tag_name in tags:
+                obj = r[tagkey]
+                if isinstance(obj,Tag):
+                    # create a duplicate Tag with updated message                                                       
+                    nobj = Tag()
+                    nobj.tag_time = obj.tag_time
+                    nobj.tag_timezone = obj.tag_timezone
+                    nobj.message = utf8_str(newmessage + "\n")
+                    nobj.name = obj.name
+                    nobj.tagger = obj.tagger
+                    nobj.object = obj.object
+                    old_id = obj.id
+                    # delete the old tag from the object store refs dictionary                                          
+                    del r.refs[_make_tag_ref(tag_name)]
+                    # remove the old annotated object itself from the object store                                      
+                    r.object_store._remove_loose_object(old_id)
+                    # add in the updated tag to the object store                                                                           
+                    r.object_store.add_object(nobj)
+                    # create a ref in the refs dictionary for the updated tag
+                    tag_id = nobj.id
+                    r.refs[_make_tag_ref(tag_name)] = tag_id             
+        os.chdir(cdir)
 
 
 def main():
