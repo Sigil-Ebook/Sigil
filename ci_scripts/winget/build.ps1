b407016a79fbfdd740be3943396eedcc9e435924
Doug Massay <douglaslmassay@gmail.com>
2023-08-05 10:54:50 -0400

    Remove 32-bit installer submission from winget [skip ci]

--- ci_scripts/winget/build.ps1
+++ ci_scripts/winget/build.ps1
@@ -14,15 +14,11 @@ function Write-MetaData {
         $Version,
         [parameter(Mandatory = $true)]
         [string]
-        $HashAmd64,
-        [parameter(Mandatory = $true)]
-        [string]
-        $Hash386
+        $HashAmd64
     )
     $content = Get-Content $FileName -Raw
     $content = $content.Replace('<VERSION>', $Version)
     $content = $content.Replace('<HASH-AMD64>', $HashAmd64)
-    $content = $content.Replace('<HASH-386>', $Hash386)
     $date = Get-Date -Format "yyyy-MM-dd"
     $content = $content.Replace('<DATE>', $date)
     $content | Out-File -Encoding 'UTF8' "./$Version/$FileName"
@@ -32,13 +28,11 @@ New-Item -Path $PWD -Name $Version -ItemType "directory"
 
 # Get hash for installers
 Invoke-WebRequest "https://github.com/Sigil-Ebook/Sigil/releases/download/$Version/Sigil-$Version-Windows-x64-Setup.exe" -OutFile 'INSTALLER-AMD64.exe'
-Invoke-WebRequest "https://github.com/Sigil-Ebook/Sigil/releases/download/$Version/Sigil-$Version-Windows-Setup.exe" -OutFile 'INSTALLER-386.exe'
 $HashAmd64 = (Get-FileHash -Path 'INSTALLER-AMD64.exe' -Algorithm SHA256).Hash
-$Hash386 = (Get-FileHash -Path 'INSTALLER-386.exe' -Algorithm SHA256).Hash
 
 # Update the metadata for the files
 Get-ChildItem '*.yaml' | ForEach-Object -Process {
-    Write-MetaData -FileName $_.Name -Version $Version -HashAmd64 $HashAmd64 -Hash386 $Hash386
+    Write-MetaData -FileName $_.Name -Version $Version -HashAmd64 $HashAmd64
 }
 if (-not $env:WINGET_TOKEN) {
     Write-Error "You must set the WINGET_TOKEN environment variable to create a pull request."
