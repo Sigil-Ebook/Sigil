4ec7f1950d2184f7482d73bd0e032229bbb21ab3
Kevin Hendricks <kevinhendricks@users.noreply.github.com>
2023-08-21 23:05:49 -0400

    add in updated ts file that missed 2.0.0

--- src/Resource_Files/ts/sigil_sr.ts
+++ src/Resource_Files/ts/sigil_sr.ts
@@ -224,7 +224,7 @@
     <message>
         <location filename="../../Dialogs/ReportsWidgets/AllFilesWidget.cpp" line="312"/>
         <source>PDF</source>
-        <translation type="unfinished"/>
+        <translation>PDF</translation>
     </message>
     <message>
         <location filename="../../Dialogs/ReportsWidgets/AllFilesWidget.cpp" line="317"/>
@@ -3621,18 +3621,18 @@ mended.</source>
     <message>
         <location filename="../../Form_Files/PGeneralSettingsWidget.ui" line="570"/>
         <source>Preview Print Settings</source>
-        <translation type="unfinished"/>
+        <translation>Подешавања приказа пре штампања</translation>
     </message>
     <message>
         <location filename="../../Form_Files/PGeneralSettingsWidget.ui" line="576"/>
         <location filename="../../Form_Files/PGeneralSettingsWidget.ui" line="579"/>
         <source>Skip Print Preview</source>
-        <translation type="unfinished"/>
+        <translation>Прескочи приказ пре штампања</translation>
     </message>
     <message>
         <location filename="../../Form_Files/PGeneralSettingsWidget.ui" line="588"/>
         <source>Print DPI:</source>
-        <translation type="unfinished"/>
+        <translation>Резолуција штамања у DPI:</translation>
     </message>
     <message>
         <location filename="../../Dialogs/PreferenceWidgets/GeneralSettingsWidget.cpp" line="222"/>
