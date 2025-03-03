ec3ea7ef25d176601cfc1ad23927019cf7a313a7
kevinhendricks <kevinhendricks@github.com>
2025-03-03 11:04:49 -0500

    a lame excuse to ...  [deploy]

--- src/Dialogs/SpellcheckEditor.cpp
+++ src/Dialogs/SpellcheckEditor.cpp
@@ -1,6 +1,6 @@
 /************************************************************************
 **
-**  Copyright (C) 2015-2024 Kevin B. Hendricks, Stratford Ontario Canada
+**  Copyright (C) 2015-2025 Kevin B. Hendricks, Stratford Ontario Canada
 **  Copyright (C) 2012-2013 John Schember <john@nachtimwald.com>
 **  Copyright (C) 2012-2013 Dave Heiland
 **
