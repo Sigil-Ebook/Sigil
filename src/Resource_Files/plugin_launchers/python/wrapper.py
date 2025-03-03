a4d8528cef405144176bea29305447fac6cacd5e
kevinhendricks <kevinhendricks@github.com>
2025-03-02 19:12:46 -0500

    prepare a new beta for testers [deploy]

--- src/Resource_Files/plugin_launchers/python/wrapper.py
+++ src/Resource_Files/plugin_launchers/python/wrapper.py
@@ -35,7 +35,7 @@ from hrefutils import ext_mime_map, mime_group_map
 
 import unicodedata
 
-_launcher_version = 20250131
+_launcher_version = 20250331
 
 _RS = chr(30) # record separator
 _US = chr(31) # unit separator
